2c3636fe5418caff17440376a66280e89cc1a33a
DJ Stomp <85457381+DJStompZone@users.noreply.github.com>
2023-07-22 16:51:49 -0700

    Update main.py
    Define various error and failure conditions and respond accordingly

--- main.py
+++ main.py
@@ -1,97 +1,87 @@
-from flask import Flask, request, jsonify, abort, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from waitress import serve
-from functools import wraps
+import logging
 import os
+import time
+from typing import Tuple, Union
 
-# Initialize Flask app
+from flask import Flask, jsonify, render_template, request
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from waitress import serve
+from werkzeug.wrappers import Response
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize SQLAlchemy and Marshmallow
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-# Your secret API key
-API_KEY = os.environ.get("API_KEY")
-
+logging.basicConfig(filename='transaction.log', level=logging.INFO)
 
 class User(db.Model):
-  discord_id = db.Column(db.String(), primary_key=True)
+  id = db.Column(db.String(100), primary_key=True)
   credits = db.Column(db.Integer, default=3)
+  last_transaction = db.Column(db.Integer, default=int(time.time()))
+
+  def __repr__(self):
+    return f"User('{self.id}', '{self.credits}', '{self.last_transaction}')"
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
 
   class Meta:
     model = User
-    load_instance = True
 
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-
-def require_api_key(view_function):
-
-  @wraps(view_function)
-  def decorated_function(*args, **kwargs):
-    if request.headers.get('Authorization') and request.headers.get(
-        'Authorization') == API_KEY:
-      return view_function(*args, **kwargs)
-    else:
-      abort(401)
-
-  return decorated_function
+API_KEY = os.environ.get('API_KEY')
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
   return render_template('index.html')
 
 
-@app.route('/api/user', methods=['GET'])
-@require_api_key
-def get_users():
-  users = User.query.all()
-  return users_schema.jsonify(users)
-
-
-@app.route('/api/user/<discord_id>', methods=['GET'])
-@require_api_key
-def get_user(discord_id):
-  user = User.query.get(discord_id)
-  if user is None:
-    user = User(discord_id=discord_id, credits=3)
-    db.session.add(user)
-    db.session.commit()
-  return user_schema.jsonify(user)
-
-
-@app.route('/user/<id>', methods=['PUT'])
-def update_user(id):
-  key = request.headers.get('Authorization')
-  if key and key == API_KEY:
+@app.route('/user/<id>', methods=['GET', 'PUT'])
+def get_user(id: str) -> Union[Response, Tuple[Response, int]]:
     user = User.query.get(id)
-    if user:
-      new_credits = user.credits + request.json.get('value', 0)
-      if new_credits < 0:
-        return jsonify({'error': 'Insufficient credits'}), 402
-      else:
-        user.credits = new_credits
-        db.session.commit()
+    if request.method == 'GET':
+        if not user:
+            user = User(id=id)
+            db.session.add(user)
+            db.session.commit()
         return user_schema.jsonify(user)
+    elif request.method == 'PUT':
+        key = request.headers.get('Authorization')
+        if key and key == API_KEY:
+            value = 0
+            if request.json:
+                value = request.json.get('value', 0)
+            if not user:
+                return jsonify({'error': 'User not found'}), 404
+            elif user.credits + value < 0:
+                return jsonify({'error': 'Insufficient credits'}), 402
+            else:
+                user.credits += value
+                user.last_transaction = int(time.time())
+                db.session.commit()
+                return user_schema.jsonify(user)
+        else:
+            return jsonify({'error': 'Unauthorized'}), 401
     else:
-      return jsonify({'error': 'User not found'}), 404
-  else:
-    return jsonify({'error': 'Unauthorized'}), 401
+        return jsonify({'error': 'Method not allowed'}), 405
+
+
+
+@app.before_request
+def log_request_info():
+  logging.info('Headers: %s', request.headers)
+  logging.info('Body: %s', request.get_data())
 
 
 if __name__ == '__main__':
   with app.app_context():
     db.create_all()
-  host=os.environ.get("FLASK_HOST") or "0.0.0.0"
-  port=os.environ.get("FLASK_PORT") or 0
+  host = os.environ.get("FLASK_HOST") or "0.0.0.0"
+  port = os.environ.get("FLASK_PORT") or 0
   serve(app, host=host, port=port)
